e4de2049a34b6f256bda00419b5306b8e2aaa44e
Nyunt Sein <nyuntsein.dev@gmail.com>
2025-07-09 00:16:39 -0400

    add some css in Profile.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import React from 'react'
+import Gallery from "./components/Gallery";
 
 const App = () => {
   return (
     <div>
       <h1>George Learing Mode.......</h1>
-      
+      <div>
+        <Gallery />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,4 @@
+import "../index.css"
 export default function Profile() {
   return <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart" />;
 }
